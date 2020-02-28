f01514a30c5cf2f9b3bb2ad54bf0bc260cd2bb91
Kyle Warren <qarren@gmail.com>
2020-02-28 15:13:28 -0800

    react-scripts@5.0.0-so1

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -38,6 +38,8 @@ const getCacheIdentifier = require('react-dev-utils/getCacheIdentifier');
 // @remove-on-eject-end
 const createEnvironmentHash = require('./webpack/persistentCache/createEnvironmentHash');
 
+const minifyClassNames = require('./css-loader-minify-classnames');
+
 // Source maps are resource heavy and can cause out of memory issue for large source files.
 const shouldUseSourceMap = process.env.GENERATE_SOURCEMAP !== 'false';
 
@@ -143,6 +145,7 @@ module.exports = function (webpackEnv) {
             config: false,
             plugins: !useTailwind
               ? [
+                  require('@spaced-out/postcss-flexbugs-strict'),
                   'postcss-flexbugs-fixes',
                   [
                     'postcss-preset-env',
@@ -536,7 +539,7 @@ module.exports = function (webpackEnv) {
                   : isEnvDevelopment,
                 modules: {
                   mode: 'local',
-                  getLocalIdent: getCSSModuleLocalIdent,
+                  getLocalIdent: isEnvProduction ? minifyClassNames() : getCSSModuleLocalIdent,
                 },
               }),
             },
