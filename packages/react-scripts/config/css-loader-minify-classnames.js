1f32347f5859266f2ba51f7bd7fec3639fc9721f
Kyle Warren <qarren@gmail.com>
2020-03-03 11:57:01 -0800

    fixed classnames minifier for classname prefixes

--- packages/react-scripts/config/css-loader-minify-classnames.js
+++ packages/react-scripts/config/css-loader-minify-classnames.js
@@ -9,7 +9,7 @@ const createMinifier = () => {
 
     let name = map.get(key);
     if (!name) {
-      name = numberHash(index++);
+      name = numberHash(index++, defaultCharacters, defaultPrefixCharacters);
       map.set(key, name);
     }
 
@@ -22,20 +22,26 @@ module.exports = createMinifier;
 const assert = require('assert');
 
 const defaultCharacters =
-  'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ10123456789';
+  'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_';
+const defaultPrefixCharacters =
+  'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
-function numberHash(number, characters = defaultCharacters) {
+function numberHash(number, characters = defaultCharacters, prefixCharacters) {
   assert(number >= 0, 'Attempted to hash a negative number');
 
+  prefixCharacters = prefixCharacters || characters;
+
+  let currentCharacters = prefixCharacters;
   let factor = Math.floor(number);
   let hash = '';
 
   do {
-    const remainder = factor % characters.length;
+    const remainder = factor % currentCharacters.length;
 
-    hash = characters[remainder] + hash;
+    hash = currentCharacters[remainder] + hash;
 
-    factor = Math.floor(factor / characters.length);
+    factor = Math.floor(factor / currentCharacters.length);
+    currentCharacters = characters;
   } while (factor > 0);
 
   return hash;
