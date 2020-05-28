cfe277f8bfb892b52ab2445a1825cffbabb57199
Kyle Warren <qarren@gmail.com>
2020-05-28 13:46:59 -0700

    added DEPLOY_ENV

--- packages/react-scripts/config/env.js
+++ packages/react-scripts/config/env.js
@@ -80,6 +80,8 @@ function getClientEnvironment(publicUrl) {
         // Useful for determining whether we’re running in production mode.
         // Most importantly, it switches React into the correct mode.
         NODE_ENV: process.env.NODE_ENV || 'development',
+        // Useful for determining where the code is deployed.
+        DEPLOY_ENV: process.env.DEPLOY_ENV || 'development',
         // Useful for resolving the correct path to static assets in `public`.
         // For example, <img src={process.env.PUBLIC_URL + '/img/logo.png'} />.
         // This should only be used as an escape hatch. Normally you would put
