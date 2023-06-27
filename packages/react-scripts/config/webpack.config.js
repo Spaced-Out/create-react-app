4302f7794ec94d3b20061ef012146d5ad5aad673
Marcos Ojeda <subliminal@gmail.com>
2023-06-26 20:12:32 -0700

    @spaced-out/react-scripts@5.0.1-so2
    removes css minifier (probably the one in react scripts is fine)

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -143,6 +143,7 @@ module.exports = function (webpackEnv) {
             config: false,
             plugins: !useTailwind
               ? [
+                  require('@spaced-out/postcss-flexbugs-strict'),
                   'postcss-flexbugs-fixes',
                   [
                     'postcss-preset-env',
@@ -150,6 +151,9 @@ module.exports = function (webpackEnv) {
                       autoprefixer: {
                         flexbox: 'no-2009',
                       },
+                      features: {
+                        'nesting-rules': true,
+                      }
                       stage: 3,
                     },
                   ],
@@ -256,6 +260,8 @@ module.exports = function (webpackEnv) {
       level: 'none',
     },
     optimization: {
+      // require stable hashes between builds if code is unchanged
+      realContentHash: true,
       minimize: isEnvProduction,
       minimizer: [
         // This is only used in production mode
